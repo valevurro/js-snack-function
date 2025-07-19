0854db9ea274bb4d59f3c8d9fbe4a916e9a73dc9
valevurro <vale.v.00@outlook.it>
2025-07-19 09:35:25 +0200

    Add second exercise

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -5,9 +5,21 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+function arrayName (names){
+    const initials = [];
+    
+    for (let i = 0; i < names.length; i++){
+        const name = names[i];
+        initials.push(name.charAt(0).toUpperCase());
+    }
+
+    return [initials];
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+const result = arrayName(names);
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
+
+console.log(result);
